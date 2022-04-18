1747ac91b5713c8a628ac7abeceffb2a0fc22679
Anderson Sathler <asathler@gmail.com>
2022-04-18 15:16:04 -0300

    Criação da tabela Livros

---
+++ database/seeders/livros.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class livros extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('livros')->insert([
+            'titulo' => 'O signo dos quatro',
+            'autor' => 'Sir Arthur Connan Doyle',
+            'descricao' => 'desc desc desc 1'
+        ]);
+
+        DB::table('livros')->insert([
+            'titulo' => 'Os cães de Baskerville',
+            'autor' => 'Sir Arthur Connan Doyle',
+            'descricao' => 'desc 2'
+        ]);
+
+        DB::table('livros')->insert([
+            'titulo' => 'Harry Potter e a pedra filosofal',
+            'autor' => 'J. K. Rowling',
+            'descricao' => 'desc bruxo'
+        ]);
+    }
+}
