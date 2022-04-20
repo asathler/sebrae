ea7c05bc29a0effa75c18a4fdfac3eb3fce52f61
Anderson Sathler <asathler@gmail.com>
2022-04-20 17:02:30 -0300

    Corrigindo rotas!!!

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-// use App\Http\Controllers\LivrosController;
+use App\Http\Controllers\LivrosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,14 +20,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+ * // Agora, ambos funcionam!
 Route::get('/livros', 'App\Http\Controllers\LivrosController@index');
-Route::get('/livro/{livro}', 'App\Http\Controllers\LivrosController@show');
 Route::get('/livros/create', 'App\Http\Controllers\LivrosController@create');
 Route::post('/livros', 'App\Http\Controllers\LivrosController@store');
+Route::get('/livros/{livro}', 'App\Http\Controllers\LivrosController@show');
+Route::get('/livros/{livro}/edit', 'App\Http\Controllers\LivrosController@edit');
+Route::put('/livros/{livro}', 'App\Http\Controllers\LivrosController@update');
 Route::delete('/livros/{livro}', 'App\Http\Controllers\LivrosController@destroy');
+*/
 
-// falta grava novo
-// Route::post('/livros/{livro}', 'App\Http\Controllers\LivrosController@edit');
-// update // put
-
-// Route::resource('livros', LivrosController::class);
+Route::resource('livros', LivrosController::class);
